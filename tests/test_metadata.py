145b1a73a7b07402d8541fb055d23bd8221615f9
Zhiming Wang <zmwangx@gmail.com>
2015-04-17 01:08:00 -0700

    upgrade test suite
    Now the test video has three streams (previously only one): a video
    stream, an audio stream, and a subtitle stream. Greatly improved code
    coverage.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -9,27 +9,44 @@ import unittest
 
 from storyboard import fflocate
 from storyboard.metadata import *
-from storyboard.util import humansize
+from storyboard.util import humansize, humantime
 
 
 class TestMetadata(unittest.TestCase):
 
     def setUp(self):
+        # create a mock srt subtitle file
+        _, self.srtfile = tempfile.mkstemp(prefix='storyboard-test-',
+                                           suffix='.srt')
+        with open(self.srtfile, 'w') as fd:
+            fd.write("1\n"
+                     "00:00:01,000 --> 00:00:02,000\n"
+                     "SubRip is the way to go\n")
+
+        # create video file
         _, self.videofile = tempfile.mkstemp(prefix='storyboard-test-',
-                                             suffix='.mp4')
+                                             suffix='.mkv')
         bins = fflocate.guess_bins()
         fflocate.check_bins(bins)  # error if bins do not exist
         self.ffmpeg_bin, self.ffprobe_bin = bins
         with open(os.devnull, 'wb') as devnull:
             command = [
                 self.ffmpeg_bin,
+                # video stream (320x180, pure pink)
                 '-f', 'lavfi',
                 '-i', 'color=c=pink:s=320x180:d=10',
+                # audio stream (silent)
+                '-f', 'lavfi',
+                '-i', 'aevalsrc=0:d=10',
+                # subtitle stream
+                '-i', self.srtfile,
+                # output option
                 '-y', self.videofile
             ]
             subprocess.check_call(command, stdout=devnull, stderr=devnull)
 
     def tearDown(self):
+        os.remove(self.srtfile)
         os.remove(self.videofile)
 
     def test_metadata(self):
@@ -42,9 +59,9 @@ class TestMetadata(unittest.TestCase):
         self.assertIsNone(vid.title)
         self.assertIsInstance(vid.size, int)
         self.assertEqual(humansize(vid.size), vid.size_text)
-        self.assertEqual(vid.format, 'MPEG-4 Part 14 (MP4)')
-        self.assertAlmostEqual(vid.duration, 10.0)
-        self.assertEqual(vid.duration_text, '00:00:10.00')
+        self.assertEqual(vid.format, 'Matroska')
+        self.assertLess(abs(vid.duration - 10.0), 1.0)
+        self.assertEqual(humantime(vid.duration), vid.duration_text)
         self.assertEqual(vid.dimension, (320, 180))
         self.assertEqual(vid.dimension_text, '320x180')
         self.assertAlmostEqual(vid.dar, 16/9)
@@ -53,22 +70,35 @@ class TestMetadata(unittest.TestCase):
         self.assertAlmostEqual(vid.frame_rate, 25.0)
         self.assertEqual(vid.frame_rate_text, '25 fps')
         self.assertIsInstance(vid.streams, list)
-        self.assertEqual(len(vid.streams), 1)
-        stream = vid.streams[0]
-        self.assertIsInstance(stream, Stream)
-        self.assertIsNotNone(stream.codec)
-        self.assertAlmostEqual(stream.dar, 16/9)
-        self.assertEqual(stream.dar_text, '16:9')
-        self.assertEqual(stream.dimension, (320, 180))
-        self.assertEqual(stream.dimension_text, '320x180')
-        self.assertAlmostEqual(stream.frame_rate, 25.0)
-        self.assertEqual(stream.frame_rate_text, '25 fps')
-        self.assertEqual(stream.height, 180)
-        self.assertEqual(stream.index, 0)
-        self.assertIsNotNone(stream.info_string)
-        self.assertIsNone(stream.language_code)
-        self.assertEqual(stream.type, 'video')
-        self.assertEqual(stream.width, 320)
+        self.assertEqual(len(vid.streams), 3)
+        # video stream
+        vstream = vid.streams[0]
+        self.assertIsInstance(vstream, Stream)
+        self.assertIsNotNone(vstream.codec)
+        self.assertAlmostEqual(vstream.dar, 16/9)
+        self.assertEqual(vstream.dar_text, '16:9')
+        self.assertEqual(vstream.dimension, (320, 180))
+        self.assertEqual(vstream.dimension_text, '320x180')
+        self.assertAlmostEqual(vstream.frame_rate, 25.0)
+        self.assertEqual(vstream.frame_rate_text, '25 fps')
+        self.assertEqual(vstream.height, 180)
+        self.assertEqual(vstream.index, 0)
+        self.assertIsNotNone(vstream.info_string)
+        self.assertIsNone(vstream.language_code)
+        self.assertEqual(vstream.type, 'video')
+        self.assertEqual(vstream.width, 320)
+        # audio stream
+        astream = vid.streams[1]
+        self.assertIsNotNone(astream.codec)
+        self.assertEqual(astream.index, 1)
+        self.assertIsNone(astream.language_code)
+        self.assertEqual(astream.type, 'audio')
+        # subtitle stream
+        sstream = vid.streams[2]
+        self.assertIsNotNone(sstream.codec)
+        self.assertEqual(sstream.index, 2)
+        self.assertIsNone(sstream.language_code)
+        self.assertEqual(sstream.type, 'subtitle')
         print('')
         print(vid.format_metadata())
 
--- tests/test_storyboard.py
+++ tests/test_storyboard.py
@@ -12,21 +12,38 @@ from storyboard.storyboard import *
 class TestStoryBoard(unittest.TestCase):
 
     def setUp(self):
+        # create a mock srt subtitle file
+        _, self.srtfile = tempfile.mkstemp(prefix='storyboard-test-',
+                                           suffix='.srt')
+        with open(self.srtfile, 'w') as fd:
+            fd.write("1\n"
+                     "00:00:01,000 --> 00:00:02,000\n"
+                     "SubRip is the way to go\n")
+
+        # create video file
         _, self.videofile = tempfile.mkstemp(prefix='storyboard-test-',
-                                             suffix='.mp4')
+                                             suffix='.mkv')
         bins = fflocate.guess_bins()
         fflocate.check_bins(bins)  # error if bins do not exist
         self.ffmpeg_bin, self.ffprobe_bin = bins
         with open(os.devnull, 'wb') as devnull:
             command = [
                 self.ffmpeg_bin,
+                # video stream (320x180, pure pink)
                 '-f', 'lavfi',
                 '-i', 'color=c=pink:s=320x180:d=10',
+                # audio stream (silent)
+                '-f', 'lavfi',
+                '-i', 'aevalsrc=0:d=10',
+                # subtitle stream
+                '-i', self.srtfile,
+                # output option
                 '-y', self.videofile
             ]
             subprocess.check_call(command, stdout=devnull, stderr=devnull)
 
     def tearDown(self):
+        os.remove(self.srtfile)
         os.remove(self.videofile)
 
     def test_storyboard(self):
